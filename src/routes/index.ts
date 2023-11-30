4667a2c45485f007c091e3acaef243e1784d1bc9
Arthur Lima <thurdelima@gmail.com>
2023-11-30 19:38:01 -0300

    doc: document routes categories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,48 @@ import ProductController from '../controllers/ProductController';
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a new category
+ *     description: Create a new category with the provided data.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: CategoryName
+ *     responses:
+ *       201:
+ *         description: Category created successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: categoryId
+ *               name: CategoryName
+ *       400:
+ *         description: Bad Request - Name is required
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Name is required
+ *       500:
+ *         description: Oops! An error occurred on our server. Please try again or contact support.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Oops! An error occurred on our server. Please try again or contact support.
+ */
 router.post('/categories', CategoryController.createCategory);
+
+
 /**
  * @swagger
  * /categories:
@@ -26,8 +67,143 @@ router.post('/categories', CategoryController.createCategory);
  *               error: Oops! An error occurred on our server. Please try again or contact support.
  */
 router.get('/categories', CategoryController.getAllCategory);
+
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   get:
+ *     summary: Get a category by ID
+ *     description: Retrieve a category by its unique identifier.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the category to retrieve
+ *         schema:
+ *           type: string
+ *         example: categoryId
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: categoryId
+ *               name: CategoryName
+ *       400:
+ *         description: Bad Request - Invalid category ID
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Invalid category ID
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Category not found
+ *       500:
+ *         description: Oops! An error occurred on our server. Please try again or contact support.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Oops! An error occurred on our server. Please try again or contact support.
+ */
 router.get('/categories/:id', CategoryController.getCategoryById);
+
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   put:
+ *     summary: Update a category by ID
+ *     description: Update the properties of a category using its unique identifier.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the category to update
+ *         schema:
+ *           type: string
+ *         example: categoryId
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: UpdatedCategoryName
+ *     responses:
+ *       200:
+ *         description: Category updated successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: categoryId
+ *               name: UpdatedCategoryName
+ *       400:
+ *         description: Bad Request - Invalid category ID or input data
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Invalid category ID or input data
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Category not found
+ *       500:
+ *         description: Oops! An error occurred on our server. Please try again or contact support.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Oops! An error occurred on our server. Please try again or contact support.
+ */
 router.put('/categories/:id', CategoryController.updateCategory);
+
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   delete:
+ *     summary: Delete a category by ID
+ *     description: Delete a category using its unique identifier.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the category to delete
+ *         schema:
+ *           type: string
+ *         example: categoryId
+ *     responses:
+ *       204:
+ *         description: Category deleted successfully
+ *       400:
+ *         description: Bad Request - Invalid category ID
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Invalid category ID
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Category not found
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Internal Server Error
+ */
 router.delete('/categories/:id', CategoryController.deleteCategory);
 
 
