1770c9d8df27fa070f8576e63850fc4bcaaca540
Arthur Lima <thurdelima@gmail.com>
2023-12-02 15:15:20 -0300

    chore: adjust string connection mongo

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -5,7 +5,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 
 async function connect(uri?: string): Promise<void> {
   try {
-    const dbUri = uri || process.env.MONGODB_URI || 'mongodb://arthur:arthur@localhost:27017/products_database?authSource=admin';
+    const dbUri = uri || process.env.MONGODB_URI || 'mongodb://mongo:27017/products_database?authSource=admin';
 
     await mongoose.connect(dbUri);
     console.log('Connected to MongoDB');
